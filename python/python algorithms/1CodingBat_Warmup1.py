50759210c88d06ffb15ffac54dce084ba3069c00
Mateusz <wanmatedzlewicz@gmail.com>
2022-09-15 20:29:34 +0200

    CodingBat added 15.9

---
+++ python/python algorithms/1CodingBat_Warmup1.py
@@ -0,0 +1,15 @@
+#The parameter weekday is True if it is a weekday, 
+# and the parameter vacation is True if we are on vacation.
+# We sleep in if it is not a weekday or we're on vacation. 
+# Return True if we sleep in.
+
+# sleep_in(False, False) → True
+# sleep_in(True, False) → False
+# sleep_in(False, True) → True
+
+def sleep_in(weekday, vacation):
+    if not weekday or vacation:
+        return True
+    else:
+        return False
+  # This can be shortened to: return(not weekday or vacation)
