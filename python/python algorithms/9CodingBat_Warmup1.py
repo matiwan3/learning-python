b00d81bb3206249a5c2abca2b38287f6b19b55ff
Mateusz <wanmatedzlewicz@gmail.com>
2022-09-22 22:26:32 +0200

    Update 9CodingBat_Warmup1.py

---
+++ python/python algorithms/9CodingBat_Warmup1.py
@@ -0,0 +1,11 @@
+# http://codingbat.com/python
+#1CodingBat_Warmup1.py
+
+
+# Given a string, return a new string where "not " has been added to the front.
+# However, if the string already begins with "not", return the string unchanged.
+
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
