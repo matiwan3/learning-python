9257217d209aeb56cb6610ad17f86ae7d97caa24
Mateusz <wanmatedzlewicz@gmail.com>
2022-09-24 12:04:05 +0200

    face recognition / added 24.9
    Face recognition program, at the end sums total amount of man and woman that has appeard

--- python/profile_recongition/recognition.py
+++ python/profile_recongition/recognition.py
@@ -4,9 +4,17 @@ from IPython.display import Image
 stalone_path = r'profile_recongition\sylvester stallone.png'
 oliwia_path = r'profile_recongition\oliwia brazil.jpg'
 ariana_path = r'profile_recongition\ariana grande.jpg'
-olga_path = r'profile_recongition\Olga ciemielewska.jpg'
-people_list = {stalone_path: 'Sylvester Stallone',oliwia_path: 'Olivia Brazil',ariana_path: 'Ariana Grande',olga_path: 'Olga Ciemielewska'}
-
+olga_path = r'profile_recongition\Olga C.jpg'
+mateusz_path = r'profile_recongition\Mateusz W.jpg'
+people_list = {
+            stalone_path: 'Sylvester Stallone',
+            oliwia_path: 'Olivia Brazil',
+            ariana_path: 'Ariana Grande',
+            olga_path: 'Olga Ciemielewska',
+            mateusz_path: 'Mateusz Wandzlewicz'
+}
+total_man = 0
+total_woman = 0
 counter = 1
 for x in people_list:
     print(f'person {counter}: {people_list[x]}')
@@ -18,8 +26,13 @@ for x in people_list:
         prog_bar = False
     )
     #Recognize gender and print
-    print('Gender: ' + obj['gender'])
-    
+    sex = obj['gender']
+    print('Gender: ' + sex)
+    if sex == 'Man':
+        total_man +=1
+    else:
+        total_woman += 1
+        
     #Recognize age and print
     get_age = str(obj['age'])
     print('Age: ' + get_age)
@@ -33,4 +46,5 @@ for x in people_list:
     print('Race: ' + max(races, key=races.get))
     counter += 1
     print(f'\n')
-    
+
+print(f'Total man: {total_man}\nTotal woman: {total_woman}')
