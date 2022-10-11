e9f6d27ad46760232b5cb22ebb7b53d3ea9509b3
Mateusz <wanmatedzlewicz@gmail.com>
2022-10-12 00:16:18 +0200

    changes 12.10

---
+++ python/O_python-functions/part 1/bin.py
@@ -0,0 +1,11 @@
+# Binary 
+# bin()
+
+base10 = 100
+base2 = bin(base10)
+
+base10_neg = -100
+base2_neg = bin(base10)
+
+print(base2)
+print(base2_neg)
---
+++ python/O_python-functions/part 1/bool.py
@@ -0,0 +1,10 @@
+# Boolean
+# bool()
+
+print(bool)
+print(bool(""))
+print(bool(1))
+print(bool(-1))
+print(bool([1,2]))
+print(bool({}))
+print(bool({"key": "value"}))
