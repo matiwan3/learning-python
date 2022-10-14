f7261958a8dabe00d3f3092f5fa84c2c96256a67
Mateusz <wanmatedzlewicz@gmail.com>
2022-10-14 18:59:12 +0200

    cganges 14.10

--- python/O_pytest/learn-classes3.py
+++ python/O_pytest/learn-classes3.py
@@ -1,18 +1,27 @@
 class Person:
+    
+    
     def __init__(self,name,height,weight):
         self.name = name
         self.height = height
         self.weight = weight
         
     def bmi(self):
-        BMI = (self.weight / (self.height**2)) * 10000
-        BMI.round(2)
+        calc_BMI = (self.weight / (self.height**2)) * 10000
+        BMI = round(calc_BMI,2)
         return BMI
 
 
 
 Jan = Person("Jan", 180, 82)
-
 Veronica = Person("Veronica", 168, 56)
+Mateusz = Person("Matthew", 172, 70)
 
-print(Jan.bmi())
+
+
+print(f" Veronica's BMI: {Veronica.bmi()}")
+print(f" Jan's BMI: {Jan.bmi()}")
+print(f" Matthew's BMI: {Mateusz.bmi()}")
+
+# variables = atributes
+# functions = methods
---
+++ python/O_pytest/learn-classes4.py
@@ -0,0 +1,27 @@
+class Student:
+    def check_pass_fail(self):
+        if self.marks >= 40:
+            return True
+        else:
+            return False
+        
+    def __init__(self,name, marks):
+        self.name = name
+        self.marks = marks
+        
+        
+student1 = Student("Harry", 85)
+student2 = Student("Janet", 30)
+
+print(student1.name)
+print(student1.marks)
+
+print(student2.marks)
+print(student2.name)
+
+did_pass = student1.check_pass_fail()
+print(did_pass)
+
+did_pass = student2.check_pass_fail()
+print(did_pass)
+
