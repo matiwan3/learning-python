53437c8c247dfb2768acf0797fb267621606a80e
Mateusz <wanmatedzlewicz@gmail.com>
2022-10-17 23:26:01 +0200

    added 17.10

---
+++ python/O_sorting/Counting sort.py
@@ -0,0 +1,4 @@
+# The counting sort algorithm works by first creating a list of the counts or occurrences of each unique value in the list. It then creates a final sorted list based on the list of counts.
+
+# One important thing to remember is that counting sort can only be used when you know the range of possible values in the input beforehand.
+
