c35eb23b18fbed1b45619a2a2539364b5ce53c2d
Mateusz <wanmatedzlewicz@gmail.com>
2022-10-19 14:27:08 +0200

    added 19.10

---
+++ python/O_classes/1-ClassesArticle.py
@@ -0,0 +1,17 @@
+# Python Class Methods: Class Vs. Instance Vs. Static Methods
+
+# In this article we are going to look at methods that we can use in Python classes. 
+# Methods are essentially functions within a class. 
+# In Python, we can use three (3) different methods in our classes: 
+# class methods, 
+# instance methods, 
+# and static methods. 
+# We are going to look at how each method differs from the other. 
+# Before we dive into the methods, let's look at two types of attributes that we find in classes. 
+# Understanding class attributes is crucial to understanding methods.
+
+# Class Attributes
+# What is a class attribute? A class attribute is a variable that belongs to a class. 
+# It is a property of the class. It belongs to the class rather than any specific object. 
+# This attribute is created outside of the constructor method (__init__). 
+# Below, we have a code that demonstrates how we create class attributes in a class.
