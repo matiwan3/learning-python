e5ae742a98b5b650ddaca556f5321aef169afc11
Mateusz <wanmatedzlewicz@gmail.com>
2022-11-14 15:35:46 +0100

    changes

--- python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
+++ python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
@@ -24,9 +24,26 @@ from sklearn.svm import SVR
 regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
 
-
-
 # Predicting a new result
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])).reshape(-1,1))
 
+# Visualising the SVR results
 
+# plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+# plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X).reshape(-1,1)), color='blue')
+# plt.title('Truth or Bluff (SVR)')
+# plt.xlabel('Position level')
+# plt.ylabel('Salary')
+# plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color='red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid)).reshape(-1,1)), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
