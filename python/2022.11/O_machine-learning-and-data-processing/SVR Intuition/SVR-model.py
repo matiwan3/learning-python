43ad2a5e53e91dd4110e42d4b0366c7f0b03fbf0
Mateusz <wanmatedzlewicz@gmail.com>
2022-11-14 12:37:49 +0100

    added file

--- python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
+++ python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
@@ -1,3 +1,19 @@
+# Import libs
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
+
+# Feature scaling
+
+
+
+
+
+
