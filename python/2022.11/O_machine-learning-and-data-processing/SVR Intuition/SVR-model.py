ab5d1225af17e774efc36d9520b9bbaef50ae06e
Mateusz <wanmatedzlewicz@gmail.com>
2022-11-14 15:03:58 +0100

    added lines of code

--- python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
+++ python/2022.11/O_machine-learning-and-data-processing/SVR Intuition/SVR-model.py
@@ -11,9 +11,22 @@ print(X)
 print(y)
 
 # Feature scaling
+y = y.reshape(len(y),1)
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# Training the SVR model on the whole dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X, y)
 
 
 
+# Predicting a new result
 
 
 
