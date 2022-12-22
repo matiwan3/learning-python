0ad71633223cd5f10eeb212d27dace5496ae1018
matiwan3 <wanmatedzlewicz@gmail.com>
2022-12-22 15:59:16 +0100

    addded file

---
+++ python/2022.12/SomeExamples/4.py
@@ -0,0 +1,9 @@
+
+my_string = "hannah montanna hello this is finAal chapter of this bookADWFAGAWWACWADWDKAGDLAKWDAW"
+format_string = my_string.lower().replace(' ', '')
+# print(len(format_string))
+print(f"Total letters in string: {format_string}")
+
+for x in range(len(format_string)):
+    pass
+    
