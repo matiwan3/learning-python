49bf2cf5fd7ee1edef59db896228096f4f5537e4
Mateusz <wanmatedzlewicz@gmail.com>
2023-04-26 23:25:14 +0200

    added feature

--- python/2023.04/Selenium facebook chatbot/main.py
+++ python/2023.04/Selenium facebook chatbot/main.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import getpass
 import logging
-
+import selectors
+import time 
 
 # python_site = driver.get("http://www.python.org")
 
 options = webdriver.ChromeOptions() 
-# to supress the error messages/logs
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options, executable_path='chromedriver.exe')
 driver = webdriver.Chrome()
@@ -24,8 +24,24 @@ def login_page(driver):
     username = input("Username: ")
     password = getpass.getpass('Password: ')
     print(f"Your username is: {username} and password is: {password}")
+
+def accept_cookies(driver,selectors):
+    try:
+        driver.find_element(By.XPATH, selectors.accept_cookies).click()
+    except:
+        print("Accept cookies selector error")
+
+def run_script():
+    login_page(driver)
+    accept_cookies(driver,selectors)
+    
+    # close driver
+    time.sleep(5)
     driver.close()
-login_page(driver)
+    
+if __name__ == "__main__":
+    run_script()
+
     
 
 
---
+++ python/2023.04/Selenium facebook chatbot/selectors.py
@@ -0,0 +1,1 @@
+accept_cookies = '/html/body/div[1]/div/div[6]/div[1]/div/div[2]/div[4]/button[2]'
