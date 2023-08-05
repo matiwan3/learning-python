e19949b6095233153309a1b6b02fbe5b1f665395
matiwan3 <wanmatedzlewicz@gmail.com>
2023-08-05 12:55:39 +0200

    adde bs4response to file saver

--- python/2023.08/webscraping august/ws1.py
+++ python/2023.08/webscraping august/ws1.py
@@ -10,8 +10,13 @@ URL = "https://www.geeksforgeeks.org/data-structures/"
 r = requests.get(URL)
 counterA = 0
 
-soup = BeautifulSoup(r.content, 'html5lib') # If this line cause an error
-print(soup.prettify())
+soup = BeautifulSoup(r.content, 'html5lib')
+
+# Save the prettified response to a text file
+with open('bs4response.txt', 'w', encoding='utf-8') as file:
+    file.write(soup.prettify())
+
+print("Prettified response saved to output.txt.")
 
 # soup = BeautifulSoup(r.text, "html.parser")
 # Find all occurrences of the letter 'a' in the page content
