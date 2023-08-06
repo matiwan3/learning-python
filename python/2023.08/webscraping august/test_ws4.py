7f6827996de8c62222ac6ee3a1eb9f4e70b3ccc3
Mateusz <wanmatedzlewicz@gmail.com>
2023-08-06 14:57:07 +0200

    1.1

--- python/2023.08/webscraping august/test_ws4.py
+++ python/2023.08/webscraping august/test_ws4.py
@@ -5,11 +5,12 @@ from ws4 import additions, ip_address
 def test_equation():
     assert 3==3
     
-def test_fail():
-    assert 3==4
+# def test_fail():
+#     assert 3==4
     
-def test_addition_ws4():
-    assert additions(4,5) == 3*3   
+@pytest.mark.parametrize("a,b", [(3,6), (1,8)])
+def test_addition_ws4(a,b):
+    assert additions(a,b) == 3*3   
 
 def test_host_ip():
     assert ip_address() == "192.168.1.16"
