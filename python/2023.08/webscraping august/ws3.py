012e1695585ea4a2a237049b6ad9ea382252c9e1
Mateusz <wanmatedzlewicz@gmail.com>
2023-08-07 17:00:07 +0200

    add 1

--- python/2023.08/webscraping august/ws3.py
+++ python/2023.08/webscraping august/ws3.py
@@ -1,17 +1,7 @@
 import unittest
-
-
-
 class YouCanDoEverything(unittest.TestCase):
     def test_perspective(self):
         self.assertTrue("Daily Progress" > "Daily Complaining")
-        
-        
-        
-            
+             
 if __name__=='__main__':
       unittest.main()
-      
-      
-# sth
-            
