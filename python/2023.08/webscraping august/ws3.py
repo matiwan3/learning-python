75869a0316f888a83a15b966f4ae3e051a6aa125
matiwan3 <wanmatedzlewicz@gmail.com>
2023-08-16 00:49:33 +0200

    changes

--- python/2023.08/webscraping august/ws3.py
+++ python/2023.08/webscraping august/ws3.py
@@ -1,1 +1,9 @@
-nul not found
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.books.toscrape.com/'
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html5lib')
+
+print(soup)
+# venv/bin/python ws3.py
