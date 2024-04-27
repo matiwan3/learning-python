059bf9ded297611da84239fb7af871ac6cac65c5
Mateusz <wanmatedzlewicz@gmail.com>
2024-04-27 09:46:35 +0200

    adjusted math problem solution

--- python/2024.04/mathProblem.py
+++ python/2024.04/mathProblem.py
@@ -1,16 +1,16 @@
 BALLS = 777
 def countBlackBalls():
     counter_7 = 0
-    index = 1
+    n = 1
     for x in range(0,BALLS):
-        if (index == 2 or index == 3) and '7' in str(x):
-            for y in range(0,len(str(x))):  
-                if str(x)[y] == '7':
+        if (n == 2 or n == 3) and '7' in str(x):
+            for num_index in range(0,len(str(x))):  
+                if str(x)[num_index] == '7':
                     counter_7 += 1
-        if index == 3:
-            index = 1
+        if n == 3:
+            n = 1
             continue
-        index += 1
-    print(f"Count of all '7' used on black balls equals: {counter_7}")
+        n += 1
+    print(f"Count of all digits '7' used on black balls equals: {counter_7}")
 
 countBlackBalls()
